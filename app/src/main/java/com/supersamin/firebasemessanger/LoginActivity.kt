2d7534199d82542e54126774c5f4a8e0b7511281
Rokhmat Prasetyo Nugroho <rokhmat.pn@kompas.com>
2020-04-10 13:28:15 +0700

    initial commit

---
+++ app/src/main/java/com/supersamin/firebasemessanger/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.supersamin.firebasemessanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        registerButton.setOnClickListener(){
+
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            Log.d("LogActivity", "Email is $email")
+            Log.d("LogActivity", "Password is $password")
+
+            //Firebase login
+            FirebaseAuth.getInstance().signInWithEmailAndPassword("$email", "$password")
+        }
+
+        returnTextView.setOnClickListener(){
+            finish()
+        }
+    }
+}
